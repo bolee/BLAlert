cc794c9ab4d503f9e0c9acea40f7641a4fa01567
Lee <lib@33.cn>
2018-12-21 17:20:02 +0800

    remove IQKeyBoard.

--- BLAlertDemo/Pods/Target Support Files/Pods-BLAlertDemo/Pods-BLAlertDemo-frameworks.sh
+++ BLAlertDemo/Pods/Target Support Files/Pods-BLAlertDemo/Pods-BLAlertDemo-frameworks.sh
@@ -153,12 +153,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
   install_framework "${PODS_ROOT}/Reveal-SDK/RevealServer-20/iOS/RevealServer.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
